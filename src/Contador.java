4983808c68b2c7a38aaa25ae8e24371867d5c0df
Pedro <pedrohenriquesb02@gmail.com>
2025-03-07 21:34:27 -0300

    Primeiro commit

---
+++ src/Contador.java
@@ -0,0 +1,13 @@
+public class Contador {
+    public static void main(String[] args) {
+        // TODO: importar a classe Scanner
+
+        // Requisitar os parâmetros ao usuário
+
+        // Criar a Exceção "ParametrosInvalidosException"
+
+        // Criar o método contar
+
+        // Desenvolver o método contar validando os parâmetros passados e criar a estrutura de repetição
+    }
+}
